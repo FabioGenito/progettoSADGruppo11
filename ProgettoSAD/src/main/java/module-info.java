be6f23704ed1f8ab314fcc0d5f2f6997aba3e17e
Fabio Genito <f.genito@studenti.unisa.it>
2026-05-27 14:18:23 +0200

    Delete ProgettoSAD directory

--- ProgettoSAD/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.progettosad {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    opens com.mycompany.progettosad to javafx.fxml;
-    exports com.mycompany.progettosad;
-}
