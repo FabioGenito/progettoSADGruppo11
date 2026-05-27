712c750f6c66a0014db8623e9afb15007c0b7c21
gcucc <g.cuccaro7@studenti.unisa.it>
2026-05-27 15:02:48 +0200

    aggiunta dipendenza per il database Postgres. aggiunta libreria sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.progettosad.mediaplayergruppo11 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
     opens com.progettosad.mediaplayergruppo11 to javafx.fxml;
     exports com.progettosad.mediaplayergruppo11;
 }
