e6d33c242404d29c082d9b26078389c7789b0100
gcucc <g.cuccaro7@studenti.unisa.it>
2026-05-31 02:28:24 +0200

    aggiunta dei metodi createPlaylist() e getAllPlaylists() implementati da Lara. Creazione dell'interfaccia PlaylistDAOInterface.

--- src/main/java/com/progettosad/mediaplayergruppo11/dao/PlaylistDAO.java
+++ src/main/java/com/progettosad/mediaplayergruppo11/dao/PlaylistDAO.java
@@ -5,8 +5,123 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import com.progettosad.mediaplayergruppo11.db.DatabaseManager;
 import com.progettosad.mediaplayergruppo11.exception.TrackAlreadyInPlaylistException;
+import com.progettosad.mediaplayergruppo11.model.Playlist;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PlaylistDAO {
+public class PlaylistDAO implements PlaylistDAOInterface {
+    
+    
+    /*
+    gestione della parte di inserimento che riguarda SQL
+    !! la playlist è scritta con \ \ perchè sql non è case sensitive e non 
+    riconosce la maiuscola
+    */
+    private static final String INSERT_PLAYLIST ="INSERT INTO \"playlist\"(name, image) VALUES (?, ?)";
+    private static final String SELECT_ALL_PLAYLISTS = "SELECT id, name, image FROM \"Playlist\" ORDER BY id ASC";
+
+    /**
+     * Crea una nuova playlist nel database e ne recupera l'ID generato automaticamente.
+     * * @param name  Il nome della playlist (non può essere nullo o vuoto).
+     * @param image Il percorso, URL o nome del file dell'immagine di copertina.
+     * @return Un oggetto Playlist completo dell'ID appena generato dal database.
+     * @throws IllegalArgumentException Se il nome fornito è nullo o vuoto.
+     * @throws RuntimeException In caso di errori SQL durante l'inserimento o la generazione dell'ID.
+     */
+    @Override
+    public Playlist createPlaylist(String name, String image) {
+        
+        /*
+        gestione tramite eccezione di errore di campo vuoto del nome della playlist
+        */
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Il nome della Playlist non può essere vuoto");
+        }
+
+        /*
+        connessione al Database con informazioni del database con metodi della
+        classe "DatabaseManager"
+        */
+        try (
+                Connection connection = DatabaseManager.getConnection();
+                PreparedStatement statement = connection.prepareStatement(
+                        INSERT_PLAYLIST,
+                        Statement.RETURN_GENERATED_KEYS)
+        ) {
+
+            statement.setString(1, name);
+            statement.setString(2, image); 
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creazione Playlist fallita, nessuna riga inserita.");
+            }
+            /*
+            le chiavi sono generate in modo seriale e l'aggiornamento è gestito in 
+            SQL con "id SERIAL PRIMARY KEY"
+            tramite la funzione getGeneratedKeys() gli id generati tramite 
+            SQL passati alla parte Java per la generazione dell'oggetto Playlist
+            la corretta generazione dell'id è gestita tramite eccezione
+            */
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+
+                if (generatedKeys.next()) {
+                    int generatedId = generatedKeys.getInt(1);
+                    return new Playlist(generatedId, name, image);
+                } else {
+                    throw new SQLException("ID Playlist non generato");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Errore durante la creazione Playlist nel DB", e);
+        }
+    
+    }
+
+    /**
+     * Estrae l'elenco completo di tutte le playlist salvate nel database.
+     * * @return Una lista di oggetti Playlist ordinati in base all'ID. 
+     * Se non ci sono playlist, restituisce una lista vuota.
+     * @throws RuntimeException In caso di errori SQL o di connettività.
+     */
+    @Override
+    public List<Playlist> getAllPlaylists() {
+        List<Playlist> listaPlaylist = new ArrayList<>();
+        /*
+        effettuo una connessione al database e gestisco gli errori con
+        try-with-resources che garantisce la chiusura automatica di Connessione e Statement
+        */
+        try (
+                Connection connection = DatabaseManager.getConnection();
+                PreparedStatement statement = connection.prepareStatement(SELECT_ALL_PLAYLISTS);
+                ResultSet resultSet = statement.executeQuery()
+        ) {
+            /*
+            eseguo un ciclo per memorizzare tutte le righe del database nella lista
+            aggiungendo gli elementi con parametri (id,name,image)
+            */
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String image = resultSet.getString("image");
+                Playlist playlist = new Playlist(id, name, image);
+                listaPlaylist.add(playlist);
+            }
+
+        } 
+        /*
+        gestione dell'eccezione per gli errori
+        */
+        catch (SQLException e) {
+            throw new RuntimeException("Errore durante il recupero delle playlist dal database", e);
+        }
+
+        return listaPlaylist;
+    }
 
     /**
      * Associa una traccia a una playlist all'interno della tabella playlist_tracks.
@@ -16,6 +131,7 @@ public class PlaylistDAO {
      * @throws TrackAlreadyInPlaylistException se la traccia è già presente nella playlist
      * @throws RuntimeException in caso di altri errori SQL o di connessione
      */
+    @Override
     public boolean addTrackToPlaylist(int playlistId, int trackId) {
         String sql = "INSERT INTO playlist_tracks (playlist_id, track_id) VALUES (?, ?)";
 
@@ -50,6 +166,7 @@ public class PlaylistDAO {
      * @param trackId ID della traccia da rimuovere
      * @return true se la rimozione ha successo (una riga eliminata), false altrimenti
      */
+    @Override
     public boolean removeTrackFromPlaylist(int playlistId, int trackId) {
         String sql = "DELETE FROM playlist_tracks WHERE playlist_id = ? AND track_id = ?";
 
---
+++ src/main/java/com/progettosad/mediaplayergruppo11/dao/PlaylistDAOInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.progettosad.mediaplayergruppo11.dao;
+
+import com.progettosad.mediaplayergruppo11.model.Playlist;
+import java.util.List;
+
+/**
+ *
+ * @author lara
+ */
+public interface PlaylistDAOInterface {
+    Playlist createPlaylist(String name, String image);
+    List<Playlist> getAllPlaylists();
+    boolean addTrackToPlaylist(int playlistId, int trackId);
+    boolean removeTrackFromPlaylist(int playlistId, int trackId);
+    
+}
